9717b43e329a2a016b97e20b0168726eaa5f563e
indooox <95325249+indooox@users.noreply.github.com>
2025-03-16 17:53:16 -0300

    EJERCICIOS 1 2 3 LOS ODIOO

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
+const reverseString = function(reversed) {
+    return reversed.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
