49a2a47e8a18b920954247ec7e1d9579b11a27cb
indooox <95325249+indooox@users.noreply.github.com>
2025-04-14 16:47:29 -0300

    niaaaa

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const letrasNums = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+    const strLimpio = str
+        .toLowerCase()
+        .split('')
+        .filter((letter) => letrasNums.includes(letter))
+        .join();
+
+    const reverso = strLimpio.split('').reverse().join('');
+
+    return reverso === strLimpio;
 };
 
 // Do not edit below this line
