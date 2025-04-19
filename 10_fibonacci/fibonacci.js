00359c2e6e413fcc98af983119408206d789e433
indooox <95325249+indooox@users.noreply.github.com>
2025-04-19 20:57:48 -0300

    AAA FUCK JAVASCRIPT

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let count 
+    if (typeof num !== 'number') {
+        count = parseInt(num)
+    } else {
+        count = num
+    }
 
+    if (count < 0) return 'OOPS';
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
